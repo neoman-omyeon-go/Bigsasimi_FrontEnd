ecd75c5946b19c0acc254d13b0c4d28e39e78768
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-04-02 03:38:44 +0900

    Login_demo_page

--- lib/Login.dart
+++ lib/Login.dart
@@ -16,11 +16,97 @@ class MyLogin extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Container(
-        child: Expanded(
-          child: Image.asset('assets/Login_origin.png'),
+      title: 'Login Page',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login Page'),),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/Login_origin.png"), // 배경 이미지 경로 설정
+            fit: BoxFit.cover,
+          ),
         ),
-      )
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'E-mail',
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'PassWord',
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true, // 패스워드 입력 필드
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        // "Forgot Password?" 텍스트를 클릭할 때 수행될 동작
+                        // 아이디, 비밀번호 찾기 페이지로 이동
+                      },
+                      child: Text(
+                        'Forgot Password?',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // 로그인 버튼 눌렀을 때 실행될 동작
+                  },
+                  child: Text('로그인'),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+      ),
+        bottomNavigationBar:
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Don't have an account? ",
+                style: TextStyle(color: Colors.blue),
+              ),
+              TextButton(
+                onPressed: () {
+                  // "Sign up" 텍스트를 클릭할 때 수행될 동작
+                  // 회원가입 페이지로 이동
+                },
+                child: Text(
+                  'Sign up',
+                  style: TextStyle(color: Colors.blue),
+                ),
+              ),
+            ],
+          ),
     );
   }
 }
