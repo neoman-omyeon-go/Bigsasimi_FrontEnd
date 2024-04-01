e2c2b649d0671141bc1f3e0927312dec81646c9f
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-04-02 04:04:11 +0900

    commit test in mac

--- lib/Login.dart
+++ lib/Login.dart
@@ -6,6 +6,7 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 void main(){
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.remove();
+  //s
   runApp(MyLogin());
 }
 
